30c8f5d6ca60e2996e001003f8996e62225a2e3e
Vlad Khitev <vkhitev@gmail.com>
2020-03-02 01:04:59 +0200

    Add User component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react'
 import { Todo } from 'app/Todo'
 import { increment, decrement } from 'fp-ts/lib/function'
+import { User } from 'app/User'
 
 export const App = () => {
   const [counter, setCounter] = useState(1)
@@ -18,6 +19,7 @@ export const App = () => {
       <button onClick={prevId}>Prev</button>
       <button onClick={nextId}>Next</button>
       <Todo todoId={counter.toString()} />
+      <User userId="1" />
     </div>
   )
 }
