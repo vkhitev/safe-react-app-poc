cd5fa467466686fb901bf73cdc207571fcbfea28
Vlad Khitev <vkhitev@gmail.com>
2020-03-21 19:37:46 +0200

    Add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import 'assets/main.css'
+import { addDecorator } from '@storybook/react'
+
+addDecorator(storyFn => storyFn())
---
+++ src/ui/button.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default {
+  title: 'Button',
+  component: 'button',
+}
+
+export const Primary = () => (
+  <button className="btn btn-primary">Primary button</button>
+)
+
+export const Secondary = () => (
+  <button className="btn btn-secondary">Secondary button</button>
+)
